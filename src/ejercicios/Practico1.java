77488a84b7f5c528c5e0689c500ad7822bb982e1
schildg <schildg@gmail.com>
2023-03-02 19:10:18 -0300

    Update Practico1.java
    quito linea

--- src/ejercicios/Practico1.java
+++ src/ejercicios/Practico1.java
@@ -5,7 +5,6 @@ public class Practico1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola Mundo!!");
-		agrego una linea para generar el conflicto
 
 	}
 
