45da43a85ee904bf5d565bb71871fde42340a9c5
schildg <schildg@gmail.com>
2023-03-02 19:07:07 -0300

    subo cambios
    subo cambios

--- src/ejercicios/Practico1.java
+++ src/ejercicios/Practico1.java
@@ -5,6 +5,7 @@ public class Practico1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola Mundo!!");
+		agrego una linea para generar el conflicto
 
 	}
 
